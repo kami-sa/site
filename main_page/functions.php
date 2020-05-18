525315564e129e298f10e4298b3ffd1768d6a245
kami-sa <kamisa1999@yandex.ru>
2020-05-18 22:19:08 +0300

    add get_user function

--- main_page/functions.php
+++ main_page/functions.php
@@ -1,8 +1,9 @@
 <?php
     require "../db/connect.php";
 
-    function check_cookie($db)
+    function check_cookie()
     {
+        global $db;
         $query = "SELECT * FROM users";
         $cookie = $_COOKIE['User'];
         if (isset($_COOKIE['User'])) {
@@ -16,5 +17,19 @@
         } else {
             return false;
         }
+    }
+
+    function get_user()
+    {
+        global $db;
+        $cookie = $_COOKIE['User'];
+        $query = "SELECT * FROM users LEFT JOIN pupils ON users.pupils_id = pupils.id ";
+        if (isset($_COOKIE['User'])) {
+            $query .= " WHERE users.cookie='" . $cookie . "'";
+        }
+        $result = mysqli_query($db, $query);
+        return mysqli_fetch_array($result);
+        /*Индексы возвращенного массива: id, email, pass, salt, cookie, pupils_id, is_admin, id(в таблице ученики),
+        name, surname, les_type, a_mark, g_mark, count_les, time*/
     }
     ?>
--- main_page/main.php
+++ main_page/main.php
@@ -1,10 +1,14 @@
 <?php
     require "functions.php";
     require"../db/connect.php";
-    if (check_cookie($db))
+    if (check_cookie())
     {
+        $user = get_user();
         require "menu.html";
         require "field.html";
+        //echo "<div><h1>Здравствуйте, ",$user['name'],"!</h1></div>";
+
+
     }
     else
         require "../authorization/auth.html";
