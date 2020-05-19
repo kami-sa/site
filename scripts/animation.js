7995bc3ba1c95bc3c5bf05ada76546142aa53b4c
kami-sa <kamisa1999@yandex.ru>
2020-05-20 00:20:47 +0300

    add text decoration animation

--- scripts/animation.js
+++ scripts/animation.js
@@ -16,3 +16,11 @@ $(".container").mousemove(function () {
     $(".container").show()
     $(".slide").hide()
 })
+
+$("#l1, #l2, #l3, #l4").mouseover(function () {
+    $(this).css("text-decoration","underline")
+})
+
+$("#l1, #l2, #l3, #l4").mouseout(function () {
+    $(this).css("text-decoration","none")
+})
